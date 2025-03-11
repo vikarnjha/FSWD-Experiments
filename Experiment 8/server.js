0a016aca6d5f5608ab41fbfde0f5b8dcef2b2bd0
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-11 09:33:59 +0530

    code optimized

--- Experiment 8/server.js
+++ Experiment 8/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/:name", (req, res) => {
-  res.send(`Hello, ${req.params.name}!`);
+app.use(express.json());
+
+// GET request (query parameter)
+app.get("/name", (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    return res.status(400).send("Name is required as a query parameter.");
+  }
+  res.send(`Hello, ${name}!`);
+});
+
+// POST request (body parameter)
+app.post("/name", (req, res) => {
+  const name = req.body.name;
+  if (!name) {
+    return res.status(400).send("Name is required in the request body.");
+  }
+  res.send(`Hello, ${name}!`);
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
